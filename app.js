d82297fba413b09af22f84f6789a45ddfdb585bd
lucascriado <lucaasbrandini@gmail.com>
2024-01-26 08:18:30 -0300

    feat: final base of connection to the servers

--- app.js
+++ app.js
@@ -1,11 +1,9 @@
-// Importa o módulo express
 const express = require('express');
 const fs = require('fs');
+const https = require('https');
 
-// Cria uma nova instância do express
 const app = express();
 
-// Define a rota padrão para responder com 'Hello World'
 app.get('/', (req, res) => {
   res.send('Hello World');
 });
@@ -15,10 +13,8 @@ const options = {
   cert: fs.readFileSync('./fullchain.pem')
 };
 
-// Define a porta
 const port = 9090;
 
-// Inicia o servidor na porta especificada
-app.listen(options, port, () => {
-  console.log(`Aplicação rodando em http://localhost:${port}`);
+https.createServer(options, app).listen(port, () => {
+  console.log(`Aplicação rodando em https://localhost:${port}`);
 });
